5d6cdbdbfaaf08bf2bf00af965169034f10aa733
jason <jason.addie@gmail.com>
2017-10-16 15:58:08 +0400

    still trying to get ckeditor to work

--- app/assets/javascripts/ckeditor/toolbar.js
+++ app/assets/javascripts/ckeditor/toolbar.js
@@ -1,6 +1,30 @@
 // To generate configuration: http://ckeditor.com/latest/samples/toolbarconfigurator/index.html#basic
 
 CKEDITOR.editorConfig = function( config ) {
+	/* Filebrowser routes */
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
+  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
+  config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+
+  // The location of a script that handles file uploads in the Flash dialog.
+  config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+
+  // The location of a script that handles file uploads in the Image dialog.
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures?";
+
+  // The location of a script that handles file uploads.
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+
+	config.allowedContent = true;
+
 	config.toolbarGroups = [
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
@@ -20,6 +44,4 @@ CKEDITOR.editorConfig = function( config ) {
 	];
 
 	config.removeButtons = 'Flash,Table,Smiley,SpecialChar,PageBreak,Save,NewPage,Preview,Print,Templates,Source,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Strike,Subscript,Superscript,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Styles,Format,Font,FontSize,TextColor,BGColor,ShowBlocks,About,Find,Replace';
-
-  config.extraPlugins = 'filebrowser';
 };
