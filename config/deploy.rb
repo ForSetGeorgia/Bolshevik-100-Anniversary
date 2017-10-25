d1a654fd67ad72cc1d222399614e673019ab2164
jason <jason.addie@gmail.com>
2017-10-25 10:55:05 +0400

    add pem to ssh line

--- config/deploy.rb
+++ config/deploy.rb
@@ -401,7 +401,7 @@ end
 private
 
 def sudo_ssh_cmd(task)
-  "ssh #{get_sudo_user(task)}@#{domain} -t -p #{ssh_port}"
+  "ssh -i #{identity_file} #{get_sudo_user(task)}@#{domain} -t -p #{ssh_port}"
 end
 
 def get_sudo_user(task)
